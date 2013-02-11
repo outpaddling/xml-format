1e7418ae7afb66ebf5dfdb7d7ea6efa40c4758aa
Jason Bacon <bacon4000@gmail.com>
2013-02-11 13:41:53 +0000

    git-svn-id: file:///home/bacon/SVN/xml-format/tags/xml-format-0.1@3 c50ce2db-d073-e211-95ba-0017315c2eee

--- xml-format.h
+++ xml-format.h
@@ -12,6 +12,18 @@ typedef enum
     COMMENT_TAG
 }   tag_t;
 
+#define MAX_TAGS    4096
+
+typedef struct
+{
+    size_t  sectioning_tag_count;
+    size_t  block_tag_count;
+    size_t  line_tag_count;
+    char    *sectioning_tags[MAX_TAGS];
+    char    *block_tags[MAX_TAGS];
+    char    *line_tags[MAX_TAGS];
+}   tag_list_t;
+
 #define MAX(x,y)    ((x) > (y) ? (x) : (y))
 #define MIN(x,y)    ((x) < (y) ? (x) : (y))
 
